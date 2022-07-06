ccffa031259aad4f0b8e9766df81106be93e924d
grinya0101 <grinya1994015@gmail.com>
2022-07-06 11:10:16 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener('click', onContaiterClick)
+
+
+ 
+
+const imgGallery = galleryItems.map(({preview, original, description}) => {
+    console.log(galleryItems);
+    return `<div class="gallery__item"><a class="gallery__link" href="${original}" ><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`
+
+    
+}).join(" ");
+
+gallery.insertAdjacentHTML("beforeend", ` ${imgGallery}`); 
+
+
+function onContaiterClick (evt){
+    eve.preventDefault()
+    if(evt.target.classList.contains('gallery__image')){
+        return
+      } 
+      const instance = basicLightbox.create(`
+      <img src="${evt.target.dataset.sourse}">
+  `)
+      console.log(evt.target.dataset.sourse)
+  instance.show()
+
+
+}
+   
+
